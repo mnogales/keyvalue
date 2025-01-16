f0893e59cc4d3dabd099d9fc0f9a3de53af0e979
lolo <nogales.manuel@gmail.com>
2025-01-16 14:04:39 +0000

    1st Iteration core func

---
+++ core.go
@@ -0,0 +1,26 @@
+package keyvalue
+
+import "errors"
+
+var store = make(map[string]string)
+
+var ErrorNoSuchKey = errors.New("No such key")
+
+func Put(key string, value string) error {
+	store[key] = value
+	return nil
+}
+
+func Get(key string) (string, error) {
+	value, ok := store[key]
+
+	if !ok {
+		return "", ErrorNoSuchKey
+	}
+	return value, nil
+}
+
+func Delete(key string) error {
+	delete(store, key)
+	return nil
+}
